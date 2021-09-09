b2f0d2cc337a504ccb7fd214ac7aee77dd58b256
bhavishayGulati <bhavishyagulati@gmail.com>
2021-09-10 00:12:55 +0530

    Add property layout updated

--- app/src/main/java/com/example/buildup/MainActivity.kt
+++ app/src/main/java/com/example/buildup/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.buildup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.buildup.databinding.ActivityMainBinding
+import com.example.buildup.databinding.ActivitySplashBinding
+import com.example.buildup.ui.LoginSignup.signupMobile.SignupActivity
+import com.example.buildup.ui.SplashActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,13 +20,13 @@ class MainActivity : AppCompatActivity() {
 //        const val PREFS_FILE_AUTH="prefs_auth"
 //        const val PREFS_KEY_TOKEN="token"
 //    }
-    private var _binding:ActivityMainBinding?=null
+    private var _binding:ActivitySplashBinding?=null
     private lateinit var authViewModel: AuthViewModel
 //    private lateinit var sharedPrefrences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding= ActivityMainBinding.inflate(layoutInflater)
+        _binding= ActivitySplashBinding.inflate(layoutInflater)
         authViewModel= ViewModelProvider(this).get(AuthViewModel::class.java)
 //        sharedPrefrences=getSharedPreferences(PREFS_FILE_AUTH, Context.MODE_PRIVATE)
 
@@ -30,6 +39,33 @@ class MainActivity : AppCompatActivity() {
 //            startActivity(intent)
 //
 //        }
+
+        val zoomOut = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out)
+
+        val zoomIn = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_in)
+        val bottomUp = AnimationUtils.loadAnimation(applicationContext, R.anim.bottom_anim)
+        val topDown = AnimationUtils.loadAnimation(applicationContext, R.anim.top_anim)
+
+        _binding!!.homeIcon.startAnimation(zoomOut)
+        _binding!!.homeIcon.visibility = View.VISIBLE
+
+        Handler().postDelayed({
+            _binding!!.titleText.startAnimation(bottomUp)
+            _binding!!.titleText.visibility = View.VISIBLE
+            Handler().postDelayed({
+                _binding!!.titleText.startAnimation(topDown)
+                _binding!!.titleText.visibility = View.GONE
+                _binding!!.homeIcon.startAnimation(zoomIn)
+                _binding!!.homeIcon.visibility = View.GONE
+                Handler().postDelayed({
+                    startActivity(Intent(this, SignupActivity::class.java))
+
+                }, 400)
+
+            }, 500)
+
+        }, 600)
+
 
 
     }
