4515523ed10d0d19d96054d108341984d781a2e6
bhavishayGulati <bhavishyagulati@gmail.com>
2021-09-15 00:50:43 +0530

    otp layout partially completed

--- app/src/main/java/com/example/buildup/ui/LoginSignup/loginSignupMobileGoogleHomePage/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/buildup/ui/LoginSignup/loginSignupMobileGoogleHomePage/ForgotPasswordActivity.kt
@@ -1,11 +1,26 @@
 package com.example.buildup.ui.LoginSignup.loginSignupMobileGoogleHomePage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buildup.R
+import com.example.buildup.databinding.ActivityForgotPasswordBinding
+
+
+private lateinit var _binding: ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        _binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        _binding.backBtn.setOnClickListener {
+            startActivity(Intent(this, LoginSignupActivity::class.java))
+        }
+
+        _binding.submitButton.setOnClickListener {
+            startActivity(Intent(this,))
+        }
     }
 }
